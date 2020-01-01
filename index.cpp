053ddc99a1a0f72fe7fe54049f6a90225e744dee
Ndekere254 <ndekerekiritu@gmail.com>
2020-01-01 15:03:08 +0300

    Variables in C++!

---
+++ index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>//do not need a semi-colon
+
+using namespace std;
+
+int main() {//-- issaFuctionHeader
+
+	/*int kittens = 100;
+	kittens = kittens + 1;*/
+	float kittens = 100.45f;
+
+	//cout << kittens << endl;
+	cout << "You have " << kittens << " kittens." << endl;
+}
